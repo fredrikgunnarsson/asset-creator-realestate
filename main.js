74a7207597ae785d6ef8fd2fa780331529d2bd20
Fredrik Gunnarsson <fredrik.s.gunnarsson@gmail.com>
2024-03-18 19:41:27 +0100

    Start project

---
+++ main.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hello!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p>
+      Hello again
+    </p>
+  </div>
+`
