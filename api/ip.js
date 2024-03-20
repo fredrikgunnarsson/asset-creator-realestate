2c007c0b8d6c489a72dc7419a620d40c1c491800
Fredrik Gunnarsson <fredrik.s.gunnarsson@gmail.com>
2024-03-20 20:20:00 +0100

    testing IP lookup

--- api/ip.js
+++ api/ip.js
@@ -8,27 +8,23 @@ const ip = async (req, res) => {
     return
   }
 
-  https
-    .get(url, (response) => {
-      // 4. Receive response from the geofenced website
-      // Set appropriate content type based on the response headers
-      res.setHeader('Content-Type', response.headers['content-type'])
+  // https
+  //   .get(url, (response) => {
+  //     res.setHeader('Content-Type', response.headers['content-type'])
 
-      // Pipe the data from the response to the client response
-      response.on('data', (chunk) => {
-        res.write(chunk)
-      })
+  //     response.on('data', (chunk) => {
+  //       res.write(chunk)
+  //     })
 
-      // End the response when the response stream ends
-      response.on('end', () => {
-        res.end()
-      })
-    })
-    .on('error', (err) => {
-      console.error('Error making HTTP request:', err)
-      res.status(500).send('Internal Server Error')
-    })
+  //     response.on('end', () => {
+  //       res.end()
+  //     })
+  //   })
+  //   .on('error', (err) => {
+  //     console.error('Error making HTTP request:', err)
+  //     res.status(500).send('Internal Server Error')
+  //   })
 
-  // res.status(200).send(`The provided URL is: ${url}`)
+  res.status(200).send(`The provided URL is: ${url}`)
 }
 export default ip
